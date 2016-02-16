c5a96ae83bd007fe5082567d8d0b50868e8090ba
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-02-16 22:43:29 +0100

    functional test with django

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#Zdislava chce vyzkouset novou stranku, zada adresu
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		#podiva se, jesli stranka dela to co ceka (to-do lists)
 		self.assertIn('To-Do', self.browser.title)
@@ -63,5 +63,3 @@ class NewVisitorTest(unittest.TestCase):
 
 		#vypne prohlizec
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
