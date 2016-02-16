16d928d3bdd6c6d27e8b3fd9e5d0b2890f4da17c
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-02-16 05:22:09 +0100

    functional test now checks we can input a todo item

--- functional-tests.py
+++ functional-tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,18 +15,38 @@ class NewVisitorTest(unittest.TestCase):
 		#Zdislava chce vyzkouset novou stranku, zada adresu
 		self.browser.get('http://localhost:8000')
 
-		#podiva se, jesli je v titulu to co ceka (to-do lists)
+		#podiva se, jesli stranka dela to co ceka (to-do lists)
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('hl').text
+		self.assertIn('To-Do', header_text)
 
-		#hned muze zadavat veci do seznamu
+		#je naznacino ze hned muze zadavat veci do seznamu
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_atribute('placeholder'),
+			'Enter a to-do item',
+		)
 
 		#napise 'koupit marmeladu'
+
+		inputbox.send_keys('koupit marmeladu')
 
 		#zmackne enter a stranka se aktualizuje
 		#ted ukazuje '1: koupit marmeladu'
 
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+
+		self.assertTrue(
+			any(row.text == '1: koupit marmeladu' for row in rows)
+		)
+
 		#je tam furt textbox na pridani dalsiho predmetu
 		#napise 'udelat palacinky'
+
+		self.fail("dodelat testy!!")
 
 		#stranka se aktualizuje a ted ukazuje voba predmety seznamu
 
