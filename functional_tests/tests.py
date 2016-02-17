9c5180a190717fb60bc43c2698a06bf7a4138fe3
Gogo Lejzr <gogo.lejzr@gmail.com>
2016-02-17 03:24:38 +0100

    first steps of ft for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -83,4 +83,26 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn('koupit marmeladu', page_text)
 		self.assertNotIn('koupit vino', page_text)
 
+	def test_layout_and_styling(self):
+		#zdislava jde na domovskou stranku
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		#vsimne si, ze textbox je pekne zarovnanej
+		inputbox = self.browser.find_element_by_id('id_new_item')
+
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+
+		#zance zadavat novej seznam a vidi ze i tady je pekne vycentrovanej textbox
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
 
